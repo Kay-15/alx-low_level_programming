8990af6f15a69e0c45a25257b8d8430d4b4f1686
Kay-15 <rnkhangweni@gmail.com>
2023-06-16 17:32:36 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,11 @@
  */
 int main(void)
 {
-char lowercase = 'a';
-char uppercase = 'A';
-while (lowercase <= 'z')
-{
-putchar(lowercase);
-lowercase++
-}
-while (uppercase <= 'Z')
-{
-putchar(uppercase);
-uppercase++
-}
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
 putchar('\n');
 return (0);
 }
