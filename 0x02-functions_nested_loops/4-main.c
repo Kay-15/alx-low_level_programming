2404c5a6deb44a2be8893c89b2baec311b4c2be9
Kay-15 <rnkhangweni@gmail.com>
2023-06-21 07:01:26 -0700

    main.h

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - check the code.
- *
+ * main - check the code
  * Return: Always 0.
  */
 int main(void)
@@ -10,8 +9,6 @@ int r;
 r = _isalpha('H');
 _putchar(r + '0');
 r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
 _putchar(r + '0');
 r = _isalpha(';');
 _putchar(r + '0');
